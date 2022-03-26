8d0c31f74f8220112e4af1056377036674228d30
MugeshGuru <mugeshguru29@gmail.com>
2022-03-26 18:48:06 +0530

    Build is ready to store in Master

---
+++ src/org/test/SampleGit.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class SampleGit {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello..");
+		
+		System.out.println("Hai...");
+		
+		
+	}
+}
